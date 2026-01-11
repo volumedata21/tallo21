67aa42d9527981936f3fff44702dabf137ace935
VolumeData21 <volumedata21@gmail.com>
2026-01-11 18:10:00 -0500

    sidebar veresion update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -273,7 +273,7 @@ export const Sidebar: React.FC<SidebarProps> = ({
                             <Github size={14} />
                             <span className={`${!isOpen ? 'hidden' : 'block'}`}>Git/VolumeData21</span>
                         </a>
-                        <span className={`${!isOpen ? 'hidden' : 'block'}`}>v0.3.8</span>
+                        <span className={`${!isOpen ? 'hidden' : 'block'}`}>v0.3.9</span>
                     </div>
                 </div>
             </aside>
